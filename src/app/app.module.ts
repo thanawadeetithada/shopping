47aaff3aa826a38e41870f3fa3d736a1b6b42221
thanawadeetit <thanawadee.tithada@allianz.com>
2021-02-24 22:33:25 +0700

    feat : add about-us component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,15 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { HeaderComponent } from './shared/header/header.component';
+import { AboutUsComponent } from './shared/about-us/about-us.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +25,8 @@ import { HeaderComponent } from './shared/header/header.component';
 
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule
 
   ],
   providers: [],
